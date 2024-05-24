2cc0887fad65b1c812a6d0454f312d99cf2e7844
ejcodes02 <ejppwork@gmail.com>
2024-05-24 16:19:54 +0800

    Improved bubble_sort

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -1,21 +1,17 @@
-def main():
-    """Basic implementation of sort in python using sorted or sort function"""
-    sample_array = [2, 1, 5, 4, 3]
-    print(sorted(sample_array))
-    sample_array.sort()
-    print(sample_array)
-
-    """Utilizes for loops to sort the int array"""
-    sample_array = [2, 1, 5, 4, 3]
-    for i in range(len(sample_array)):
-        for j in range(i + 1, len(sample_array)):
-            x = sample_array[i]
-            y = sample_array[j]
-            if x > y:
-                sample_array[i] = y
-                sample_array[j] = x
-
-    print(sample_array)
+def bubble_sort(arr) -> list[int]:
+    """Application of bubble sort algorithm using for loops"""
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 if __name__ == "__main__":
-    main()
+    """Basic implementation of sort in python using sorted or sort function"""
+    sample_array = [64, 34, 25, 12, 22, 11, 90]
+    # print(sorted(sample_array))
+    # sample_array.sort()
+    # print(sample_array)
+
+    """With Bubble sort algorithm"""
+    print(bubble_sort(sample_array))
