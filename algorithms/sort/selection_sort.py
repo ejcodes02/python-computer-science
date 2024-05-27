3fb731930a8c5f56cbf4fbe406ff024b1fa94a36
ejcodes02 <ejppwork@gmail.com>
2024-05-27 15:13:36 +0800

    Update selection sort

--- algorithms/sort/selection_sort.py
+++ algorithms/sort/selection_sort.py
@@ -1,7 +1,14 @@
 def selection_sort(arr: list[int]) -> list[int]:
-    ...
+    for i in range(len(arr[:-1])):
+        # x is the minimum index
+        x = i
+        for j in range(i+1, len(arr)):
+            if arr[x] > arr[j]:
+                x = j
+        arr[i], arr[x] = arr[x], arr[i]
+
     return arr
 
 if __name__ == "__main__":
-    sample_array = [2, 8, 5, 3, 9, 4, 1]
+    sample_array = [64, 34, 25, 12, 22, 11, 90]
     print(selection_sort(sample_array))
