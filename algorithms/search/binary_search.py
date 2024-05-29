9aa372a52086a1b4cb1fe867b195723bd52736b7
ejcodes02 <ejppwork@gmail.com>
2024-05-29 20:04:15 +0800

    Add binary search

---
+++ algorithms/search/binary_search.py
@@ -0,0 +1,8 @@
+def binary_search(arr: list[int], target: int) -> int:
+    target_index = ...
+    return target_index
+
+if __name__ == "__main__":
+    # Array must be sorted before binary search
+    arr = [1, 4, 5, 7, 9, 12, 15, 18, 19, 22, 25, 29, 40, 50]
+    print(binary_search(arr))
